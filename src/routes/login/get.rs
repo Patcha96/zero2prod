8724412dfc9082417f7f1e89ab6a8d7f43364bbf
Patcha96 <eryk.grzeskow@gmail.com>
2023-11-03 09:20:15 +0800

    chore: improve cookie auth

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,6 +1,7 @@
 //! src/routes/login/get.rs
 use crate::startup::HmacSecret;
-use actix_web::{http::header::ContentType, web, HttpResponse};
+use actix_web::cookie::{time::Duration, Cookie};
+use actix_web::{http::header::ContentType, web, HttpRequest, HttpResponse};
 use hmac::{Hmac, Mac};
 use secrecy::ExposeSecret;
 
@@ -24,53 +25,94 @@ impl QueryParams {
     }
 }
 
-pub async fn login_form(
-    query: Option<web::Query<QueryParams>>,
-    secret: web::Data<HmacSecret>,
-) -> HttpResponse {
-    let error_html = match query {
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html: String = match request.cookie("_flash") {
         None => "".into(),
-        Some(query) => match query.0.verify(&secret) {
-            Ok(error) => format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error)),
-            Err(e) => {
-                tracing::warn!(
-                    error.message = %e,
-                    error.cause_chain = ?e,
-                    "Failed to verify query parameters using the HMAC tag"
-                );
-                "".into()
-            }
-        },
+        Some(cookie) => {
+            format!("<p><i>{}</i></p>", cookie.value())
+        }
     };
     HttpResponse::Ok()
         .content_type(ContentType::html())
+        .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
         .body(format!(
             r#"<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>Login</title>
-</head>
-<body>
-    {error_html}
-    <form action="/login" method="post">
-        <label>Username
-            <input
-                type="text"
-                placeholder="Enter Username"
-                name="username"
-            >
-        </label>
-        <label>Password
-            <input
-                type="password"
-                placeholder="Enter Password"
-                name="password"
-            >
-        </label>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html>"#,
+        <html lang="en">
+        <head>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8">
+            <title>Login</title>
+        </head>
+        <body>
+            {error_html}
+            <form action="/login" method="post">
+                <label>Username
+                    <input
+                        type="text"
+                        placeholder="Enter Username"
+                        name="username"
+                    >
+                </label>
+                <label>Password
+                    <input
+                        type="password"
+                        placeholder="Enter Password"
+                        name="password"
+                    >
+                </label>
+                <button type="submit">Login</button>
+            </form>
+        </body>
+        </html>"#,
         ))
 }
+
+// pub async fn login_form(
+//     query: Option<web::Query<QueryParams>>,
+//     secret: web::Data<HmacSecret>,
+// ) -> HttpResponse {
+//     let error_html = match query {
+//         None => "".into(),
+//         Some(query) => match query.0.verify(&secret) {
+//             Ok(error) => format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error)),
+//             Err(e) => {
+//                 tracing::warn!(
+//                     error.message = %e,
+//                     error.cause_chain = ?e,
+//                     "Failed to verify query parameters using the HMAC tag"
+//                 );
+//                 "".into()
+//             }
+//         },
+//     };
+//     HttpResponse::Ok()
+//         .content_type(ContentType::html())
+//         .body(format!(
+//             r#"<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta http-equiv="content-type" content="text/html; charset=utf-8">
+//     <title>Login</title>
+// </head>
+// <body>
+//     {error_html}
+//     <form action="/login" method="post">
+//         <label>Username
+//             <input
+//                 type="text"
+//                 placeholder="Enter Username"
+//                 name="username"
+//             >
+//         </label>
+//         <label>Password
+//             <input
+//                 type="password"
+//                 placeholder="Enter Password"
+//                 name="password"
+//             >
+//         </label>
+//         <button type="submit">Login</button>
+//     </form>
+// </body>
+// </html>"#,
+//         ))
+// }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -73,6 +73,7 @@ pub struct TestApp {
     pub email_server: MockServer,
     pub port: u16,
     pub test_user: TestUser,
+    pub api_client: reqwest::Client,
 }
 impl TestApp {
     pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
@@ -101,7 +102,7 @@ impl TestApp {
     }
 
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .post(format!("{}/subscriptions", &self.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
@@ -111,7 +112,7 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .post(&format!("{}/newsletters", &self.address))
             // `reqwest` does all the encoding/formatting heavy-lifting for us.
             .basic_auth(&self.test_user.username, Some(&self.test_user.password))
@@ -125,11 +126,7 @@ impl TestApp {
     where
         Body: serde::Serialize,
     {
-        reqwest::Client::builder()
-            // Avoid following 303 redirect back to login page during testing
-            .redirect(reqwest::redirect::Policy::none())
-            .build()
-            .unwrap()
+        self.api_client
             .post(&format!("{}/login", &self.address))
             // This `reqwest` method makes sure that the body is URL-encoded
             // and the `Content-Type` header is set accordingly
@@ -137,6 +134,19 @@ impl TestApp {
             .send()
             .await
             .expect("Failed to execute request.")
+    }
+
+    // Our tests will only look at the HTML page, therefore
+    // we do not expose the underlying reqwest::Response
+    pub async fn get_login_html(&self) -> String {
+        self.api_client
+            .get(&format!("{}/login", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+            .text()
+            .await
+            .unwrap()
     }
 }
 
@@ -162,12 +172,19 @@ pub async fn spawn_app() -> TestApp {
 
     let _ = tokio::spawn(application.run_until_stopped());
 
+    let client = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .cookie_store(true)
+        .build()
+        .unwrap();
+
     let test_app = TestApp {
         address: format!("http://localhost:{}", application_port),
         port: application_port,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
         test_user: TestUser::generate(),
+        api_client: client,
     };
     test_app.test_user.store(&test_app.db_pool).await;
     test_app
